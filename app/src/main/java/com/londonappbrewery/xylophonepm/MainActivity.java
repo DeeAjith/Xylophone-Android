59be0359c25b1b32122d266ee3217674587ef1f0
ajith <deevannagariajith@gmail.com>
2019-01-27 20:24:18 +0530

    SoundPooled to Buttons added!

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private final float NORMAL_PLAY_RATE = 1.0f;
 
     // TODO: Add member variables here
+    private SoundPool mSoundPools;
     private int mCSoundId;
     private int mDSoundId;
     private int mESoundId;
@@ -31,15 +32,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO: Create a new SoundPool
+        mSoundPools = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS,AudioManager.STREAM_MUSIC,0);
 
 
         // TODO: Load and get the IDs to identify the sounds
 
-
+        mCSoundId = mSoundPools.load(getApplicationContext(),R.raw.note1_c,1);
+        mDSoundId = mSoundPools.load(getApplicationContext(),R.raw.note2_d,1);
+        mESoundId = mSoundPools.load(getApplicationContext(),R.raw.note3_e,1);
+        mFSoundId = mSoundPools.load(getApplicationContext(),R.raw.note4_f,1);
+        mGSoundId = mSoundPools.load(getApplicationContext(),R.raw.note5_g,1);
+        mASoundId = mSoundPools.load(getApplicationContext(),R.raw.note6_a,1);
+        mBSoundId = mSoundPools.load(getApplicationContext(),R.raw.note7_b,1);
     }
 
     // TODO: Add the play methods triggered by the buttons
 
 
-
+    public void playC(View v) {
+        mSoundPools.play(mCSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+    public void playD(View v) {
+        mSoundPools.play(mDSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+    public void playE(View v) {
+        mSoundPools.play(mESoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+    public void playF(View v) {
+        mSoundPools.play(mFSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+    public void playG(View v) {
+        mSoundPools.play(mGSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+    public void playA(View v) {
+        mSoundPools.play(mASoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+    public void playB(View v) {
+        mSoundPools.play(mBSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
 }
